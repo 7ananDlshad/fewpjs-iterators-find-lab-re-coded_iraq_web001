f55aa6b3cb54bcc6ec377b0159ed307f3ed5c695
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-06-10 19:32:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ function testFunc() {
   return "hi"
 }
 
+
 function superbowlWin(arr){
+  let result;
+  let newarr = arr.map((item) => {
+    if(item.result === "W"){
+      result = item.year
+      return result;
+    }
+  })
+  let year = newarr.find((year) => {
+    if(year === "1969"){
+      return year;
+    }
+  })
 
-
+ return year;
 }
-superbowlWin(record)
+superbowlWin(record);
