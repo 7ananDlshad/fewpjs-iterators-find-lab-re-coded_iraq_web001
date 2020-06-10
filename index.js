5b936efbd78e07507a0234189d17365b14fb1292
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-06-10 19:35:23 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arr){
+  let result;
+  let newarr = arr.map((item) => {
+    if(item.result === "W"){
+      result = item.year
+      return result;
+    }
+  })
+  
+  let year = newarr.find((year) => {
+    if(year === "1969"){
+      return year;
+    }
+  })
+
+ return year;
+}
+superbowlWin(record);
